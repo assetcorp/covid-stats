4777886b2780d68859823452e7554845e7de9758
assetCorp <michaelazumah@live.com>
2020-03-19 10:00:02 +0000

    Handle environment variables appropriately

--- server/utils/db.js
+++ server/utils/db.js
@@ -29,20 +29,3 @@ const connectToDatabase = async ( uri, dbName ) => {
 }
 
 export const getDatabase = async ( dbName = null ) => await connectToDatabase( process.env.MONGODB_URI, dbName )
-
-// The main, exported, function of the endpoint,
-// dealing with the request and subsequent response
-export default async ( req, res ) => {
-	// Get a database connection, cached or otherwise,
-	// using the connection string environment variable as the argument
-	const db = await connectToDatabase( process.env.MONGODB_URI )
-
-	// Select the "users" collection from the database
-	const collection = await db.collection( 'users' )
-
-	// Select the users collection from the database
-	const users = await collection.find( {} ).toArray()
-
-	// Respond with a JSON string of all users in the collection
-	res.status( 200 ).json( { users } )
-}
