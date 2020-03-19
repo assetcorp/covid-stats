a3adff8f032c0acacaf3ce9df4741938809fab2c
assetCorp <michaelazumah@live.com>
2020-03-19 09:48:11 +0000

    Use try cacth

--- server/utils/db.js
+++ server/utils/db.js
@@ -8,20 +8,24 @@ let cachedDb = null
 // A function for connecting to MongoDB,
 // taking a single parameter of the connection string
 const connectToDatabase = async ( uri, dbName ) => {
-	// If the database connection is cached,
-	// use it instead of creating a new connection
-	if ( cachedDb ) return cachedDb
+	try {
+		// If the database connection is cached,
+		// use it instead of creating a new connection
+		if ( cachedDb ) return cachedDb
 
-	// If no connection is cached, create a new one
-	const client = await MongoClient.connect( uri, { useNewUrlParser: true } )
+		// If no connection is cached, create a new one
+		const client = await MongoClient.connect( uri, { useNewUrlParser: true } )
 
-	// Select the database through the connection,
-	// using the database path of the connection string
-	const db = await client.db( dbName === null ? url.parse( uri ).pathname.substr( 1 ) : dbName )
+		// Select the database through the connection,
+		// using the database path of the connection string
+		const db = await client.db( dbName === null ? url.parse( uri ).pathname.substr( 1 ) : dbName )
 
-	// Cache the database connection and return the connection
-	cachedDb = db
-	return db
+		// Cache the database connection and return the connection
+		cachedDb = db
+		return db
+	} catch ( error ) {
+		console.log( error, uri )
+	}
 }
 
 export const getDatabase = async ( dbName = null ) => await connectToDatabase( process.env.MONGODB_URI, dbName )
